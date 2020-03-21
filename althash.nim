d6bf63d5c0eb168ee8ca27e4f1a97ddb34a53655
Charles Blake <charlechaud@gmail.com>
2020-03-21 10:45:29 -0400

    Add some more hashes including the very strong Wang Yi one (that remains quite fast at least on amd64/x86_64 where imul gives you easy access to the upper part of 128-bit product).  Provide hashWY0, too, though it is not quite as scrambling.

--- althash.nim
+++ althash.nim
@@ -18,13 +18,16 @@
 ## discard half our key entropy, just rotating down 32 bits, though bit reversal
 ## might be better (though expensive) and ``shr`` reduction from hcode to table
 ## address is another way out.  In general achieving "high order avalanche" is
-## likley more natural than the low order avalanche needed for ``and mask``.
+## likely more natural than the low order avalanche needed for ``and mask``.
 ## Both may count as what https://burtleburtle.net/bob/hash/integer.html calls
 ## "half avalanche".  Anyway, rotating is portably fast (often 1 cycle latency,
 ## 1-per cycle tput for an immediate constant number of bits).  Bit reversal is
 ## not so slow using a fully unrolled loop and 16 entry nibble lookup table.
 ## Different hashes will perform more/less well on different data.  So, we just
 ## provide a few here, and one is based upon highly non-linear bit reversal.
+##
+## The strongest hash is hashWY which passes all of SMHasher's entropy tests
+## and so is the default Hash-rehasher.
 ##
 ## (Incidentally, most "fast in GB/s" hashes are far too slow for just one int.
 ## Even assessing them that way for lookup tables is misleading.  You want time
@@ -44,25 +47,63 @@ proc hashRoMu1*[T: int16|uint16](x: T): Hash {.inline.}  =
 proc hashRoMu1*[T: int32|uint32](x: T): Hash {.inline.} =
   Hash(rotateRightBits(uint64(x) * 0xd3833e81'u64, 31))
 
-proc hashRoMu1*(x: int64|uint64): Hash {.inline.} =
+proc hashRoMu1*(x: int64|uint64|Hash): Hash {.inline.} =
   ## 1-hop of Romul-DuoJr using x as seed
   Hash(rotateLeftBits(x * 15241094284759029579'u64, 27))
+
+proc hashRoMu2*(x: int64|uint64|Hash): Hash {.inline.} =
+  ## Just the cross terms of 128-bit whole product
+  # (h*s + l) * (a*s + b) = h*a*s*s + h*b*s + l*a*s + l*b.  Constants taken
+  # from roMuDuoJr;  Could probably be tuned to get near avalanche.
+  let lo = uint64(x) and 0xFFFFFFFF'u64
+  let hi = uint64(x) shr 32
+  Hash(rotateLeftBits(0xD3833E80'u64 * lo  +  hi * 0x4F4C574B'u64, 27))
+
+#XXX should do 1B, 2B, 4B versions as well.
+proc HiXorLo(A, B: uint64): uint64 {.inline.} =
+  # Xor of high & low 8B of full 16B product
+  when defined(gcc) or defined(llvm_gcc) or defined(clang):
+    {.emit: """__uint128_t r = A; r *= B; return (r >> 64) ^ r; """.}
+  elif defined(windows) and not defined(tcc):
+    {.emit: """A = _umul128(A, B, &B); return A ^ B;""".}
+  else: # Fall back for weaker platforms/compilers, e.g. Nim VM, etc.
+    let
+      ha = A shr 32
+      hb = B shr 32
+      la = A and 0xFFFFFFFF'u64
+      lb = B and 0xFFFFFFFF'u64
+      rh  = ha * hb
+      rm0 = ha * lb
+      rm1 = hb * la
+      rl  = la * lb
+      t   = rl + (rm0 shl 32)
+    var c = if t < rl: 1'u64 else: 0'u64
+    let lo = t + (rm1 shl 32)
+    c += (if lo < t: 1'u64 else: 0'u64)
+    let hi = rh + (rm0 shr 32) + (rm1 shr 32) + c
+    return hi xor lo
+
+proc hashWY*(x: int64|uint64|Hash): Hash {.inline.} =
+  ## Wang Yi's hash for 8B ints. (Outer HiXorLo(foo, const) may help v.little.)
+  const P0 = 0xa0761d6478bd642f'u64
+  const P1 = 0xe7037ed1a0b428db'u64
+  const P5 = 0xeb44accab455d165'u64
+  let x = uint64(x)
+  Hash(HiXorLo(HiXorLo(P0, x xor P1), 8 xor P5))
+
+proc hashWY0*(x: int64|uint64|Hash): Hash {.inline.} =
+  ## A slightly simplified/early version of Wang Yi's hash for 8B ints.
+  ## Faster, but less well vetted/tested by e.g. SMHasher.
+  const P0 = 0xa0761d6478bd642f'u64
+  const P1 = 0xe7037ed1a0b428db'u64
+  let x = uint64(x)
+  Hash(HiXorLo(P0, x xor P1))
 
 proc hashRevFib*(x: int32|uint32): Hash {.inline.} =
   Hash(reverseBits(uint32(x) * 0xd3833e81'u64))
 
 proc hashRevFib*(x: int64|uint64): Hash {.inline.} =
   Hash(reverseBits(uint64(x) * 15241094284759029579'u64))
-
-proc hashWang*(x: int32|uint32): Hash {.inline.} =
-  let u = uint32(x) # inspired from https://gist.github.com/badboy/6267743
-  let x = u xor ((u shr 20) xor (u shr 12))
-  result = Hash((x xor (x shr 7) xor (x shr 4)))
-
-proc hashWang*(x: int64|uint64): Hash {.inline.} =
-  let u = uint64(x) # inspired from https://gist.github.com/badboy/6267743
-  let x = u xor ((u shr 20) xor (u shr 12))
-  result = Hash((x xor (x shr 7) xor (x shr 4)))
 
 proc secureSalt*(x: pointer): Hash {.inline.} =
   proc getrandom(buf: pointer, len: csize_t, flags: cuint): csize {. importc:
@@ -86,11 +127,9 @@ else:
   proc hashRevFib*(x: int|uint): Hash {.inline.} = hashRevFib(uint32(x))
 
 when defined(unstableHash):
-  proc hash*(hashValue, salt: Hash): Hash {.inline.} =
-    hashRoMu1(hashValue) xor Hash(salt)
+  proc hash*(hsh, salt: Hash): Hash {.inline.} = hashWY(hsh) xor Hash(salt)
 else:
-  proc hash*(hashValue, salt: Hash): Hash {.inline.} =
-    hashRoMu1(hashValue)
+  proc hash*(hsh, salt: Hash): Hash {.inline.} = hashWY(hsh)
 
 when defined(hashDebug):
   template dbg*(x) = x
