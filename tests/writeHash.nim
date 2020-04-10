b9b5321d511df00c0c0f9f652fec8851cdccc1b9
Charles Blake <charlechaud@gmail.com>
2020-04-10 11:18:30 -0400

    Add a benchmarking mode.

--- tests/writeHash.nim
+++ tests/writeHash.nim
@@ -1,14 +1,23 @@
 # Just writes 8 byte binary hashes of 0..<n to stdout for input to PractRand.
-import althash, bitops, cligen, cligen/osUt
+import althash, bitops, times, cligen, cligen/osUt
+
 type HashFun = enum WangYi, MoreMur, NASAM
-proc writeHash(n=99, r=0, fun=WangYi) =
-  var h: Hash
+
+proc writeHash(n=99, r=0, fun=WangYi, bench=false) =
+  var h, sum: Hash
+  let t0 = getTime()
   for i in 0 ..< n:
     let x = rotateLeftBits(uint64(i), r)
     case fun
     of WangYi:  h = hashWangYi1(x)
     of MoreMur: h = hashMoreMur(x)
     of NASAM:   h = hashNASAM(x)
-    discard stdout.uriteBuffer(cast[cstring](h.addr), Hash.sizeof.Natural)
+    if bench:
+      sum += h
+    else:
+      discard stdout.uriteBuffer(cast[cstring](h.addr), Hash.sizeof.Natural)
+  if bench:
+    echo "sum: ", sum, " ns/hash: ", (getTime() - t0).nanoseconds.float / n.float
+
 dispatch(writeHash, help = { "n": "samples", "r": "rotation",
                              "fun": "hash function"})
