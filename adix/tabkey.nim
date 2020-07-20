c06ede32f58c9367d5cc34dfb94c546bddf041d1
Charles Blake <charlechaud@gmail.com>
2020-07-20 10:25:42 -0400

    Go ahead and inline the wrapper around `cmp`.

--- adix/tabkey.nim
+++ adix/tabkey.nim
@@ -5,8 +5,8 @@ type Pair*[K, V] = tuple[key: K, val: V]
 proc hash*[K, V](e: Pair[K, V]): Hash {.inline.} = hash(e.key)
 proc `==`*[K, V](a, b: Pair[K, V]): bool {.inline.} = a.key == b.key
 
-proc cmpKey*[K, V](a, b: Pair[K, V]) = cmp(a.key, b.key)  # For sorting
-proc cmpVal*[K, V](a, b: Pair[K, V]) = cmp(a.val, b.val)
+proc cmpKey*[K, V](a, b: Pair[K, V]) {.inline.} = cmp(a.key, b.key) # To sort
+proc cmpVal*[K, V](a, b: Pair[K, V]) {.inline.} = cmp(a.val, b.val)
 
 proc high*[K, V](e: Pair[K, V]): K {.inline.} = e.key.high
 proc low*[K, V](e: Pair[K, V]): K {.inline.} = e.key.low
