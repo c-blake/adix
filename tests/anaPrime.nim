9af740ca7d026db8af224c1dd3cf6ddc2988ede2
Charles Blake <charlechaud@gmail.com>
2020-08-21 07:47:12 -0400

    Rename `product` to `sig`.  Time whole build.  Comment about future.

--- tests/anaPrime.nim
+++ tests/anaPrime.nim
@@ -1,7 +1,7 @@
 import strutils, times, iltab, althash, cligen, cligen/[mfile, mslice]
 proc hash(x: uint64): Hash {.inline.} = hashRoMu1(x) # =~ 1.03x faster
 
-proc product(word: MSlice): uint64 {.inline.} =
+proc sig(word: MSlice): uint64 {.inline.} = # word signature
   const prime = [ #9/267751 oflow
     7'u64, 61, 41, 53, 2, 71, 47, 29, 3, 97, 89, 17, 59,
     19, 5, 31, 101, 11, 13, 23, 37, 79, 73, 67, 43, 83 ]
@@ -9,18 +9,19 @@ proc product(word: MSlice): uint64 {.inline.} =
   for ch in word: result *= prime[ord(ch) - ord('A')]
 
 proc qry(dict="words", stats=false, query: seq[string]) =
+  let t0 = getTime()
   let mf = mopen(dict)
   if mf == nil: return
-  let t0 = getTime()
+  # tables should allow file-backed allocation to make saving answer easy
   var ana = initILTab[uint64, uint32, 0](mf.len div 10, numer=3, denom=1)
   var wds = newSeqOfCap[MSlice](mf.len div 10)
   for word in mf.mSlices:
-    ana.add word.product, uint32(wds.len)
+    ana.add word.sig, uint32(wds.len)
     wds.add word
   let t1 = getTime()
   for word in query:
     let word = word.toUpperAscii
-    let prod = word.toMSlice.product
+    let prod = word.toMSlice.sig
     echo word, ":"
     for ana in ana.allValues(prod):
       echo "  ", wds[ana]
