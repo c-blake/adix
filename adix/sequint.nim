d064754726ec7bba2c8f5007f72ae772b2a7c933
Charles Blake <charlechaud@gmail.com>
2020-09-02 09:51:58 -0400

    Add `setLen` and `add`.  Rename `elen` field to just `len`.

--- adix/sequint.nim
+++ adix/sequint.nim
@@ -1,33 +1,29 @@
 ## This module provides a memory optimized ``seq[uint]`` for a user-given range
 ## of numbers (by default its own length).  E.g., if the range is 0..63, it uses
 ## just 6 bits per number (plus rounding error).  In the best cases this allows
-## packing numbers up to about 2x (e.g., 64/33) more densely.  The public API
-## uses only the widest type, usually a 64-bit unsigned integer.  To store n
-## indices (default mode) takes only n*ceil(lg(n)) bits.  24*2**24/8 is only 48
-## MiB and circa 2020 L3 caches have become that large.  Full 21-bit unicode can
-## fit in 2.78 MiB and 14..17 bit spaces fit in typical L1/L2.
+## packing numbers up to ~2x (e.g., 16/9) more densely.  The public API uses
+## only the widest type, usually a 64-bit unsigned integer.  To store ``n``
+## indices from ``0..n-1`` takes ``n*ceil(lg(n))`` bits.  E.g., circa 2020 L3
+## CPU caches have become large enough to support  24*2**24/8 = 48 MiB.
 ##
 ## Using the wide type for backing store and limiting the design to hold only
 ## numbers up to said wide type ensures <= 2 consecutive backing items are
-## needed to form any given number.  We also assume the almost universal pattern
+## needed to get any given number.  We also assume the almost universal pattern
 ## that the number of bits in that widest type is a power of 2 like 16, 32, 64.
-## Note that ISO/IEC JTC1 SC22 WG14 n2472 makes sizes of exact-bit ints the
-## next power of two, and so would be of no help in this array context.
+## {NOTE: ISO/IEC JTC1 SC22 WG14 n2472 makes sizes of exact-bit ints next po2.}
 
-#XXX There is a `bitslice`/`bitsliced` feature in bleeding edge Nim that could
-#    make this code look quite a bit cleaner, but it seems all debugged now.
-import bitop #, strformat
+import bitop
 const iBit = 8 * sizeof(int)
 const iShf = lgPow2(iBit)
 const iMsk = (1 shl iShf) - 1
 
 type
-  SeqUint* = object
+  SeqUint* = object   ## Space-optimized ``seq[uint]``
     data: seq[uint]   # Backing store, probably 64-bit ints
-    elen: uint        # number of bits-bit elements allocated
-    bits: int8
+    len: int          # number of elements occupied
+    bits: int8        # size of elements in bits
 
-proc roundUp(sz: int): int {.inline.} =
+proc roundUp(sz: int): int {.inline.} = # number of int-sized words for sz elts
   let m = sz and iMsk
   if m == 0: return sz
   return sz + iBit - m
@@ -36,26 +32,23 @@ proc bits*(s: SeqUint): int {.inline.} = int(s.bits)
 
 proc low*(s: SeqUint): int {.inline.} = 0
 
-proc len*(s: SeqUint): int {.inline.} = int(s.elen)
-
-proc high*(s: SeqUint): int {.inline.} = int(s.elen) - 1
+proc high*(s: SeqUint): int {.inline.} = int(s.len) - 1
 
 proc clear*(s: var SeqUint) {.inline.} =
   zeroMem s.data[0].addr, s.data.len * s.data[0].sizeof
 
-#TODO Does not yet support resize, setLen, add, etc.
-#TODO Could potentially optimize 8,16,32 bit cases with CPU supported types
-proc init*(s: var SeqUint, initialSize, numBound=0) {.inline.} =
+#TODO Could potentially optimize 8,16,32-bit cases with CPU supported types
+proc init*(s: var SeqUint, initialSize=0, numBound=0) {.inline.} =
   let bits = if   numBound    > 0: lg(numBound)
              elif initialSize > 0: lg(initialSize)
              else: 0
   let bitsz = initialSize * bits
   if bitsz > 0:
     s.data.setLen (roundUp(bitsz) shr iShf)
-    s.elen = uint(initialSize)
+    s.len  = 0
     s.bits = bits.int8
 
-proc initSeqUint*(initialSize, numBound=0): SeqUint {.inline.} =
+proc initSeqUint*(initialSize=0, numBound=0): SeqUint {.inline.} =
   result.init(initialSize, numBound)
 
 # Consider storing 3 bit numbers packed into 8 bit words big-endian-wise like:
@@ -110,6 +103,16 @@ proc `[]=`*(s: var SeqUint, i: int|uint, x: int|uint) {.inline.} =
     let cMask = (1'u shl (iBit - wdmod)) - 1
     s.data[wdix]   = ((x and cMask) shl wdmod) or (w0 and loM)
     s.data[wdix+1] = (w1 and not oMask) or (x shr w0bit)
+
+proc setLen*(s: var SeqUint, size: int) {.inline.} =
+  let bitsz = size * s.bits
+  s.data.setLen (roundUp(bitsz) shr iShf)
+  s.len  = size
+
+proc add*(s: var SeqUint, v: uint) {.inline.} =
+  let i = s.len
+  s.setLen i + 1
+  s[i] = v
 
 iterator items*(s: SeqUint): uint =
   for i in 0 ..< s.len: yield s[i]
