7a0981bc302e813297f177199590b711dc3e2a6d
Charles Blake <charlechaud@gmail.com>
2020-09-02 08:27:50 -0400

    The beginning of the big overhaul re-org to "most tunable full generics" specialized for common, untuned cases.

--- adix/iltab.nim
+++
@@ -1,7 +0,0 @@
-import tabkey, deftaz, ilset
-
-defTab(ILTab, ILSet, il)
-
-when isMainModule:
-  proc initTab(): auto = initILTab[uint32, uint8, 0](4)
-  include tests/testTab
--- adix/ottab.nim
+++
@@ -1,7 +0,0 @@
-import tabkey, deftab, otset
-
-defTab(OTTab, OTSet, ot)
-
-when isMainModule:
-  proc initTab(): auto = initOTTab[uint32, uint8](4)
-  include tests/testTab
