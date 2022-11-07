a7ca9e438cdd5c36ed2463f1b0c8b019b273f6c0
Charles Blake <charlechaud@gmail.com>
2022-11-07 09:22:42 -0500

    Overhaul to generalize from mean+-itsStderr to any number of easy stats I.e. any quantile, min, max, n, IQRs, etc.  Put each on its own row in `--table` mode or delimit with `join` in 1-row mode.  Expand special delimiter value for header stripping from "s" to "strip".  Update docs.
    As part of this work, re-orient the non-varying -> unparsed string logic
    to happen at stat,nums -> string conversion time since it now depends on
    what kind of stat -- a variation measure or valuation measure (which also
    meant nums[j].len > 1 became > 0).
    
    Prepare in the code to rename the file from `mnsd.nim` to `cstats.nim`.

--- util/mnsd.nim
+++ util/mnsd.nim
@@ -1,60 +1,87 @@
-from std/math     import sqrt
-from labFloats    import labFloats, initSep
-from adix/stat    import basicStats, BasicStats
-from cligen/strUt import fmtUncertain
-from std/strutils import join, strip
+import std/[math,strutils,algorithm],cligen/strUt, adix/stat,fitl/qtl, labFloats
 when not declared(stdin): import std/syncio
 
-proc mnsd*(delim="white", table="", hsep="s", pm="+-", exp = -2..4,
-           unity="$val0 $pm $err0", sci="($valMan $pm $errV)$valExp", nd=2) =
-  ## This consumes any stdin looking like repeated/regular intercalary text with
-  ## numbers embedded & produces a 1-line summary with the last such intercalary
-  ## text and mean+-stderr of each *column* of floats.  If `table!=""`, contexts
-  ## are joined via `hsep` into headers for the associated reduced numbers, with
-  ## columns separated by `table` (e.g. ',').
-  let (labs, nums) = labFloats(stdin, initSep(delim))
+proc needToSort(stats: seq[string]): bool =
+  for ex in stats: (if ex.startsWith("q"): return true)
+
+proc valueSpace(ex: string): bool = ex.startsWith("q") or ex.startsWith("m")
+
+proc cstats*(delim="white", table="", hsep="strip", pm="+-", exp = -2..4, nd=2,
+             unity="$val0 $pm $err0", sci="($valMan $pm $errV)$valExp",
+             join=",", stats: seq[string]) =
+  ## This consumes any stdin looking like regular intercalary text with embedded
+  ## floats & prints a summary with the LAST such text & requested `stats` for
+  ## any varying float column. If `table!=""`, context is joined via `hsep` into
+  ## headers for associated reduced numbers, with columns separated by `table`
+  ## (eg. ',').  Available stats (ms if none given)..
+  ##   mn: mean      sd: sdev      se: stderr(mean) (i.e. sdev/n.sqrt)
+  ##   sk: skewness  kt: kurtosis  ms: mn +- se via `pm exp nd unity sci` params
+  ##   iq: interQuartileRange      sq: semi-interQuartileRange   n: len(nums)
+  ##   qP: General Parzen interpolated quantile P (0<=P<=1 float; 0=min; 1=max)
+  ## ..print as separate rows in the `table` mode or else joined by `join`.
+  let stats = if stats.len == 0: @["ms"] else: stats
+  let doSort = stats.needToSort
+  var (labs, nums) = labFloats(stdin, initSep(delim))
   strUt.pmDfl = pm
-  if table.len > 0:
-    # TEST: regularTrailing regularLeading allNumbers irregTrailing irregLeading
+
+  proc toStr(ex, con: string, bs: BasicStats[float], x: seq[float]): string =
+    if bs.min == bs.max:                # Numeric but non-varying
+      if ex.valueSpace: con else: "0"   # value space=>unparsed; variation=>"0"
+    elif ex.startsWith("q") and(let q=parseFloat(ex[1..^1]); 0.0<=q and q<=1.0):
+      $x.quantile(q)                    # TODO precision/format controls?
+    else: # Could pre-parse expressions, but many columns seem unlikely
+      case ex 
+      of "mn": fmtUncertain(bs.mean, bs.sdev/bs.n.float.sqrt,"$val0","$valMan$valExp",exp,nd)
+      of "sd": $bs.sdev                 # TODO should do only nd digits
+      of "se": fmtUncertain(bs.mean, bs.sdev/bs.n.float.sqrt,"$err0","$errV$valExp",exp,nd)
+      of "ms": fmtUncertain(bs.mean, bs.sdev/bs.n.float.sqrt, unity,sci,exp,nd)
+      of "sk": $x.skewness              # TODO should do only nd digits
+      of "kt": $x.kurtosis              # TODO should do only nd digits
+      of "iq": $(x.quantile(0.75) - x.quantile(0.25))
+      of "sq": $((x.quantile(0.75) - x.quantile(0.25))*0.5)
+      of "n" : $x.len
+      else: "unknownOp"
+
+  if table.len > 0:                     # Multi-row format for data reduction
     var hdr: string
-    var hdrs, cols: seq[string]
-    let doStrip = hsep == "s"
+    var hdrs: seq[string]
+    var rows = newSeq[seq[string]](stats.len)
+    let doStrip = hsep == "strip"
     let hsep = if doStrip: "" else: hsep
-    for j, ctxt in labs:
-      if nums[j].len > 1:
+    for j, con in labs:
+      if nums[j].len > 0:               # Numeric
         if hdr.len > 0:
           hdrs.add if doStrip: hdr.strip else: hdr
           hdr.setLen 0
         let bs = nums[j].basicStats
-        if bs.sdev > 1e-14*bs.mean:             # Varied
-          cols.add fmtUncertain(bs.mean, bs.sdev/bs.n.float.sqrt,
-                                unity, sci, exp, nd)
-        else: cols.add ctxt                     # Constant
-      else:                                     # Non-Numeric
+        if doSort: nums[j].sort
+        for i, ex in stats: rows[i].add ex.toStr(con, bs, nums[j])
+      else:                             # Non-Numeric
         if hdr.len > 0: hdr.add hsep
-        hdr.add ctxt
-    if hdr.len > 0:                             # Last header may be empty
+        hdr.add con
+    if hdr.len > 0:                     # Last header may be empty
       hdrs.add if doStrip: hdr.strip else: hdr
     stdout.write join(hdrs, table), "\n"
-    stdout.write join(cols, table), "\n"
-  else:
-    for j, ctxt in labs:
-      if nums[j].len > 1:
+    for r in rows: stdout.write r.join(table), "\n"
+  else:                                 # Single row format for data reduction
+    for j, con in labs:
+      if nums[j].len > 0:               # Numeric
         let bs = nums[j].basicStats
-        if bs.sdev > 1e-14*bs.mean:             # Varied
-          stdout.write fmtUncertain(bs.mean, bs.sdev/bs.n.float.sqrt,
-                                    unity, sci, exp, nd)
-        else: stdout.write ctxt                 # Constant
-      else: stdout.write ctxt                   # Non-Numeric
+        if doSort: nums[j].sort
+        for i, ex in stats:
+          if i > 0: stdout.write join
+          stdout.write ex.toStr(con, bs, nums[j])
+      else: stdout.write con            # Non-Numeric
     stdout.write "\n"
 
-when isMainModule: import cligen; dispatch mnsd, help={
+when isMainModule: import cligen; dispatch cstats, help={
   "help"       : "print cligen-erated help",    # narrower than
   "help-syntax": "prepend, plurals..",          #..the defaults
   "delim"      : "inp delims; Repeats=>fold",
   "table"     :"""labels -> header of a
 `table`-separated table""",
-  "hsep"       : "header sep | strip if==s",
+  "hsep"       : "header sep|strip if=strip",
+  "join"       : "intern st-delim for 1-row",
   "pm"         : "plus|minus string",
   "unity"      : "near unity format",
   "sci"        : "scientific format",
