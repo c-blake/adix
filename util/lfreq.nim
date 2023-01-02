8696fc8d62bd96040f5ebc399a174e8898a29173
Charles Blake <charlechaud@gmail.com>
2023-01-02 12:22:50 -0500

    Re-organize CLI operation to be more like current `tests/wf.nim`.

--- util/lfreq.nim
+++ util/lfreq.nim
@@ -1,8 +1,9 @@
 when not declared(stdin): import std/[syncio, formatfloat]
-import adix/lptabz {.all.}, cligen, cligen/[mslice, osUt]
+import adix/lptabz {.all.}, cligen, cligen/[mslice, osUt], std/times
 
-proc lfreq(size=1000, n=0, count=false, grand=false, Norm=false) =
+proc lfreq(n=0, count=false, Norm=false, size=1000, tm=false) =
   ## Histogram `stdin` lines (read w/non-memory mapped IO to be pipe friendly).
+  let t0 = epochTime()
   var cnt  = initLPTab[MSlice, int](size) # HCell 16+8+8=32B
   var nTot = 0
   var str  = ""                 # A big stack of string data
@@ -18,22 +19,20 @@ proc lfreq(size=1000, n=0, count=false, grand=false, Norm=false) =
       cnt.cell(i).key = MSlice(mem: str[off].addr, len: ms.len)
       cnt.cell(i).val = 1
   if count:
-    echo cnt.len," uniqueLines ",nTot," totalLines"; return
-  if grand:                     # Write early so invoker can..
-    stderr.write nTot, '\n'     #..normalize with 1 less pass.
-  template output =
+    echo cnt.len," unique ",nTot," total"
+  template o =
     if Norm: stdout.urite c.float / nTot.float
     else: stdout.urite c
     stdout.urite " "; stdout.urite k; stdout.urite "\n"
-  if n == 0: (for (k, c) in pairs(cnt): output())
-  else: (for (k, c) in cnt.topByVal(n): output())
+  if  n == 0: (for (k, c) in pairs(cnt): o())       # unsorted
+  elif n > 0: (for (k, c) in cnt.topByVal(n): o())  # top n
+  if tm: stderr.write epochTime() - t0, " sec\n"    # n<0 => only `c`/tm
 
 when isMainModule: dispatch lfreq, help={
-  "size" : "pre-size hash table for size unique entries",
   "n"    : "only emit most frequent `n` lines (!=0=>sorted)",
   "count": "only emit counts: unique & grand total",
-  "grand": "total line count -> stderr (BEFORE main output)",
-  "Norm" : "normalize frequencies by dividing by grand tot"}
+  "Norm" : "normalize frequencies by dividing by grand tot",
+  "size" : "pre-size hash table for size unique entries"}
 
 #NOTE: 16B=Eg hc:30,off:34,len:30,cnt:34 has reasonable limits & if 1/2 very rnd
 # access memory saves you from a mem.hierarchy cliff that can matter by 2..1000X
