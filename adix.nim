7d74a5262b1146e70ea4d32bd524fa3852498411
Charles Blake <charlechaud@gmail.com>
2023-03-18 08:58:13 -0400

    Add some more recent modules to doc generation.

--- adix.nim
+++ adix.nim
@@ -1,16 +1,20 @@
 when defined(nimdoc):
   import adix/althash
+  import adix/amoft
   import adix/bist
   import adix/bitop
   import adix/bltab
   import adix/btree
+  import adix/cumsum
   import adix/ditab
   import adix/lghisto
   import adix/lptabz
   import adix/memutil
   import adix/metab
+  import adix/nsort
   import adix/sequint
   #import adix/stat
+  import adix/tdigest
   import adix/xlang
 else:
   {.error: "use `import adix/{module of interest}`".}
