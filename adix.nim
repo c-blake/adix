4d3d99a1d10601f907359a08c449dcc052b0c187
Charles Blake <charlechaud@gmail.com>
2024-09-29 22:22:55 -0400

    Add some missing imports of newer modules to the nim doc driver.

--- adix.nim
+++ adix.nim
@@ -5,16 +5,20 @@ when defined(nimdoc):
   import adix/bitop
   import adix/bltab
   import adix/btree
+  import adix/cpuCT
   import adix/cumsum
   import adix/ditab
   import adix/lghisto
   import adix/lptabz
   import adix/memutil
   import adix/metab
+  import adix/mvstat
   import adix/nsort
+  import adix/oats
   import adix/sequint
   import adix/stat
   import adix/tdigest
+  import adix/topk
   import adix/uniqce
   import adix/xlang
 else:
