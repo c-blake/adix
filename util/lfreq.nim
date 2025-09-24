2b1109fc03b53c3002c38c17ced7b05b98525cc0
Charles Blake <charlechaud@gmail.com>
2025-09-24 05:36:39 -0400

    Add `frecent` build mode to score by "frecency" (a simpler, 1-parameter notion than the one popularized by firefox).  This entailed:
      - add some comments to explain new impl
      - make an abstract counter type `Counter` that can be `float32`
      - change `used` test to `off != 0`
      - rename `nTot` -> `wTot` for more generic weight total, lifting
        updates from main loop into `incFailed`
      - Migrate `1` to `w` for the next weight bump.
      - add EWMA age logic to `incFailed`.  *Would* be trivial `w *= wMul`
        for short time series or unbounded floating point reprs, but since
        neither of those is very likely, rescale when nearing FP overflow.
      - add an `old` parameter to control per-point retention/decay rate
      - `s.setLen 1` (w/a comment) so that new `used` works correctly
      - track rename of `nTot` to `wTot`
      - re-order elif clauses to match doc-comment k,c,f order.
      - add when-branch to fmt raw weight in 7-sig-fig scientific notation
      - Track uint32 -> Counter
      - Add documentation for new `old` parameter
    
    This tests out fine on sample data as well as lowercased words from
    Tale Of Two Cities.  With --old=1, numbers match integer counting mode
    & speed is within 2% (time ratio on TOTC = 1.0216 +- 0.0025 on i7).
    That last fact suggests maybe just always doing FP arithmetic instead
    and just old=1.0 default with a more expansive documentation comment.

--- util/lfreq.nim
+++ util/lfreq.nim
@@ -3,71 +3,85 @@ import std/[hashes,times,strutils], adix/oats, cligen/[mslice,strUt,osUt],cligen
 
 const bLen {.intdefine.} = 16   # <16K long;  RT params better but more work
 const bOff {.intdefine.} = 32   # <4G UNIQUE line data
-const bCnt {.intdefine.} = 32   # <4 GiCount
+const bCnt {.intdefine.} = 32   # <4 GiCount; Must be 32 for -d=frecent
 type
+  Counter = (when defined frecent: float32 else: uint32)
   Count {.packed.} = object     # Dense-ish hash Count type
     when defined hashCache: hc: uint32 # 10B|14B per cell
     len {.bitsize: bLen.}: uint32 # Cmp goes hc, len, key
     off {.bitsize: bOff.}: uint32
-    cnt {.bitsize: bCnt.}: uint32
+    when defined frecent: cnt: Counter
+    else                : cnt {.bitsize: bCnt.}: Counter
   Counts = object
     dat: seq[Count]
     nUsed: int
 
 var s: string; oatKStack s, Counts, Count, off,uint32, MSlice, MSlice
 proc key(c: Counts, i: int): MSlice = c.dat[i].key
-proc val(c: var Counts, i: int, v: uint32) {.used.} = c.dat[i].cnt = v
-proc val(c: Counts, i: int): uint32 = c.dat[i].cnt
-proc used(c: Counts, i: int): bool = c.dat[i].cnt != 0
+proc val(c: var Counts, i: int, v: Counter) {.used.} = c.dat[i].cnt = v
+proc val(c: Counts, i: int): Counter = c.dat[i].cnt
+proc used(c: Counts, i: int): bool = c.dat[i].off != 0
 when defined hashCache:         # 2nd def triggers saving lpt behavior
   proc hash(ms: MSlice): Hash = mslice.hash(ms).uint32.Hash
   proc hash(c: var Counts, i: int, hc: Hash) {.used.} = c.dat[i].hc = hc.uint32
   proc hash(c: Counts, i: int): Hash = c.dat[i].hc.Hash
 oatCounted c,Counts, c.nUsed; oatSeq Counts, dat  # make counted & resizable
-#when Counts is VROat[MSlice, MSlice, uint32]: {.warning: "Counts is a VROat"}
+#when Counts is VROat[MSlice, MSlice, Counter]: {.warning: "Counts is a VROat"}
 
+var wMul = 1/0.99       # Set by CLI driver; Used only in "frecent" mode
+var wTot = Counter(0)   # Total weight used only for --count & @f normalization
+var w    = Counter(1)   # Constant if not frecent, else exponentially growing
+let wMax = 3.4028235e33 # 38 = max float32; Leave 1e5 room for `wTot`
 proc incFailed(h: var Counts, ms: MSlice): bool =
   var ms = ms
   if ms.len > (1 shl bLen) - 1: # Careful to not overflow XXX rate limit msgs
     erru "truncating too long (", $ms.len, ") line: ", ($ms)[0..<128], "...\n"
   h.upSert(ms, i):              # Found key @i:
-    if h.dat[i].cnt == (1 shl bCnt) - 1:
+    if (when defined frecent: false else: h.dat[i].cnt == (1 shl bCnt) - 1):
       erru "counter overflow for: ",$ms,"\n" # no update XXX rate limit msgs
-    else: h.dat[i].cnt.inc      #   bump
+    else: h.dat[i].cnt += w; wTot += w  # bump
   do:                           # Novel key->i:
     h.dat[i].off = s.add(ms, (1 shl bOff) - 1):
       erru "unique line data overflow at:",$ms,"\n" #XXX rate limit msgs
       return true               # Cannot go on GLOBALLY
-    h.dat[i].len = ms.len.uint32# Init
-    h.dat[i].cnt = 1u32
+    h.dat[i].len = ms.len.uint32 # Init
+    h.dat[i].cnt = w; wTot += w
+  when defined frecent:         # Do 1-param frecency idea; Simpler than firefox
+    w *= wMul                   # Always-grow-for-new-data EWMA update
+    if w > wMax:                # Nearing per-key weight repr limit..
+      let sclInv = 1.0/wMax     # *= twice since this pushes FP repr limits
+      for c in mitems h.dat: c.cnt *= sclInv; c.cnt *= sclInv
+      w *= sclInv; w *= sclInv        # These *='s move all FP numbers from..
+      wTot *= sclInv; wTot *= sclInv  #..near top of repr to near bottom.
 
 proc lfreq(n=10, count=false, size=9999, dSize=81920, recTerm='\n',
-           format="@c @k", RecTerm="\n", tm=false) =
+           format="@c @k", RecTerm="\n", old=0.99, tm=false) =
   ## Histogram `stdin` lines (read w/non-memory mapped IO to be pipe friendly).
   ## Limits: <4 GiB unique data; <16 KiB lines; <4 GiCount.
   let t0 = if tm: epochTime() else: 0.0
   var h: Counts; h.setCap size  # pre-size table & data
-  s.setLen dSize; s.setLen 0
-  var nTot = 0
+  s.setLen dSize; s.setLen 1    # `1` here lets us encode empty as 0-offset
+  when defined frecent: wMul = 1.0/old
   block IO:
     for (line, nLine) in stdin.getDelims(recTerm):
       let ms = MSlice(mem: line, len: nLine - 1)
-      inc nTot                  # Always bump `nTotal`
       if h.incFailed(ms): break IO
-  if count: outu h.len," unique ",nTot," total ",s.len," B\n"
-  let nInv = 1.0/nTot.float; var cs, fs: string # Setup for..
+  if count: outu h.len," unique ",wTot," total ",s.len," B\n"
+  let wInv = 1.0/wTot.float; var cs, fs: string # Setup for..
   let prs = format.tmplParsed('@')              #..nice output
   template output =
     for (id, arg, call) in prs:
       if id.idIsLiteral: outu MSlice(mem: format[arg.a].addr, len: arg.len)
-      elif format[id.a] == 'c': cs.setLen 0; cs.addInt c; outu cs
       elif format[id.a] == 'k': outu k
-      elif format[id.a] == 'f': fs.setLen 0; fs.fcvt c.float*nInv, 9; outu fs
+      elif format[id.a] == 'c':
+        when defined frecent: cs.setLen 0; cs.ecvt c.float, 6; outu cs
+        else                : cs.setLen 0; cs.addInt c; outu cs
+      elif format[id.a] == 'f': fs.setLen 0; fs.fcvt c.float*wInv, 9; outu fs
       else: outu MSlice(mem: format[call.a].addr, len: call.len)
     outu RecTerm
   if   n == 0: (for (k, c) in pairs(h): output())
-  elif n > 0 : (for (k, c) in topByVal[MSlice,MSlice,uint32](h, n): output())
-  elif n < -1: (for (k, c) in topByVal[MSlice,MSlice,uint32](h, -n, order=Descending): output())
+  elif n > 0 : (for (k, c) in topByVal[MSlice,MSlice,Counter](h, n): output())
+  elif n < -1: (for (k, c) in topByVal[MSlice,MSlice,Counter](h, -n, order=Descending): output())
   if tm: stderr.write epochTime() - t0, "\n"  # -n-1 for only time output
 
 when isMainModule: dispatch lfreq, help={
@@ -78,4 +92,5 @@ when isMainModule: dispatch lfreq, help={
   "recTerm": "input record terminator",
   "RecTerm": "output record terminator",
   "format" : "output format: @k=key @c=count @f=fraction",
+  "old"  : "exponen.weight for 'old' ages (if frecent)",
   "tm"   : "emit wall time of counting to stderr & quit"}
